51ba7082f1de7a4a3238f261873a0e409a20a308
Laszlo Dobos <dobos@complex.elte.hu>
2014-09-12 16:29:58 +0200

    Per instrument detector classes added.

---
+++ src/Herschel.Lib/DetectorPacsPhoto.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Jhu.Spherical;
+
+namespace Herschel.Lib
+{
+    public class DetectorPacsPhoto : Detector
+    {
+        public override Cartesian[] Corners
+        {
+            get
+            {
+                double a = 1.75 / 60.0;
+                double b = 0.875 / 60.0;
+
+                return new Cartesian[]
+                {
+                    new Cartesian(a, b),
+                    new Cartesian(-a, b),
+                    new Cartesian(-a, -b),
+                    new Cartesian(a, -b)
+                };
+            }
+        }
+
+        /// <summary>
+        /// Returns the footprint of the detector
+        /// </summary>
+        /// <returns></returns>
+        public override Region GetFootprint(Cartesian pointing, double pa)
+        {
+            return GetFootprintRectangle(pointing, pa);
+        }
+    }
+}
---
+++ src/Herschel.Lib/DetectorSpirePhoto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Jhu.Spherical;
+
+namespace Herschel.Lib
+{
+    public class DetectorSpirePhoto : Detector
+    {
+        public override Cartesian[] Corners
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public override Region GetFootprint(Cartesian pointing, double pa)
+        {
+            return GetFootprintRectangle(pointing, pa);
+        }
+    }
+}
