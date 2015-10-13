2136616aa25c44d454d9a856a5c728963a1cfaab
Laszlo Dobos <dobos@complex.elte.hu>
2015-10-13 16:36:55 +0200

    Verify scripts updated.

--- sql/verify_nonscan.sql
+++ sql/verify_nonscan.sql
@@ -5,24 +5,60 @@ ORDER BY 1, 2, 3
 
 /*
 inst	obsType	pointingMode	(No column name)
-1	1	2	13				-- raster mode
+1	1	2	13				-- raster mode		TODO
 1	1	8	18872			-- scan map			OK
-1	1	65	2185
-1	1	66	23
-1	2	1	5068
-1	2	4	1088
-2	1	1	88
-2	1	8	1013
-2	1	16	6020
-2	1	65	328
-2	2	0	24
+1	1	65	2185			-- chop-nod			TODO
+1	1	66	23				-- chop-nod raster	calib only
+1	2	1	5068			-- spec
+1	2	4	1088			-- spec
+2	1	1	88				-- pointed			calib only
+2	1	8	1013			-- scan map			OK
+2	1	16	6020			-- scan map			OK
+2	1	65	328				-- chop-nod			mostly cal, single point
+2	2	0	24				-- spec, no pointing		calib only
 2	2	1	2129
 2	2	2	22
-4	1	8	856
+4	1	8	856				--					OK
 */
+
+-- PACS raster maps
 
 SELECT * FROM Observation
 WHERE inst = 1 AND obsType = 1 AND pointingMode = 2
 
-SELECT *
-FROM RasterMap
+SELECT * FROM load.RawObservation WHERE inst = 1
+	AND obsID IN 
+	(
+	SELECT obsid FROM Observation
+	WHERE inst = 1 AND obsType = 1 AND pointingMode = 2
+	)
+
+SELECT * FROM load.RawPointing WHERE inst = 1 AND obsID = 1342184600
+
+SELECT BBID, COUNT(*)
+FROM load.RawPointing WHERE inst = 1 AND obsID = 1342184600
+GROUP BY BBID
+
+----------*****************--------------
+
+SELECT * FROM Observation
+WHERE inst = 1 AND obsType = 1 AND pointingMode = 66
+
+
+SELECT * FROM load.RawObservation WHERE inst = 1
+	AND obsID IN 
+	(
+	SELECT obsid FROM Observation
+	WHERE inst = 1 AND obsType = 1 AND pointingMode = 66
+	)
+
+SELECT BBID, COUNT(*)
+FROM load.RawPointing WHERE inst = 1 AND obsID = 1342183560
+GROUP BY BBID
+
+---------------************---------------
+
+-- SPIRE, pointing mode 0???
+
+SELECT * FROM Observation
+WHERE inst = 2 AND obsType = 2 AND pointingMode = 0
--- sql/verify_spectro.sql
+++ sql/verify_spectro.sql
@@ -1,8 +1,36 @@
+-- Count all spectro observations grouped by pointing mode
+
 SELECT inst, obsType, pointingMode, COUNT(*)
 FROM Observation
 WHERE obsType = 2
 GROUP BY inst, obsType, pointingMode
 ORDER BY 1, 2, 3
+
+/*
+inst	obsType	pointingMode	(No column name)
+1	2	1	5068		-- pointed
+1	2	4	1088		-- mapping?
+2	2	0	24			-- calibration?
+2	2	1	2129		-- pointed
+2	2	2	22			-- raster -- lesz hozzá új
+*/
+
+-- Count those only which have pointing info associated
+
+SELECT inst, obsType, pointingMode, COUNT(*)
+FROM Observation
+WHERE obsType = 2 AND obsID IN (SELECT obsID FROM load.RawPointing WHERE inst IN (1, 2))
+GROUP BY inst, obsType, pointingMode
+ORDER BY 1, 2, 3
+
+/*
+inst	obsType	pointingMode	(No column name)
+1	2	1	4982
+1	2	4	1074
+2	2	0	24
+2	2	1	2129
+2	2	2	15
+*/
 
 -------------------------------------
 
@@ -11,6 +39,14 @@ ORDER BY 1, 2, 3
 SELECT TOP 100 *
 FROM Observation
 WHERE inst = 1 AND obsType = 2 AND pointingMode = 1
+
+-- PACS pointed spectro with no pointing
+SELECT *
+FROM Observation
+WHERE inst = 1 AND obsType = 2 AND pointingMode = 1 AND
+	obsID NOT IN (SELECT obsID FROM load.RawPointing WHERE inst IN (1,2))
+
+-- 86 with no pointing at all
 
 SELECT *
 FROM load.RawPointing
@@ -51,6 +87,36 @@ SELECT *
 FROM load.RAwObservation
 WHERE inst = 2 AND obsType = 2 AND pointingMode = 1
 
+-- Number of pointings per obs
+
+SELECT p.obsID, COUNT(*)
+FROM Observation o
+INNER JOIN load.RawPointing p ON p.inst = o.inst AND p.obsID = o.obsID
+WHERE o.inst = 2 AND o.obsType = 2 AND o.pointingMode = 1
+GROUP BY p.obsID
+
+-- Observations with more than two pointings
+
+SELECT * FROM Observation
+WHERE inst = 2 AND obsID IN
+(
+	SELECT p.obsID
+	FROM Observation o
+	INNER JOIN load.RawPointing p ON p.inst = o.inst AND p.obsID = o.obsID
+	WHERE o.inst = 2 AND o.obsType = 2 AND o.pointingMode = 1
+	GROUP BY p.obsID
+	HAVING COUNT(*) != 2
+)
+ORDER BY obsID
+
+--
+
+SELECT * FROM Observation
+WHERE inst = 2 AND obsID = 1342245117
+
+SELECT * FROM load.RawPointing
+WHERE inst = 2 AND obsID = 1342245117
+
 SELECT *
 FROM load.RawPointing
 WHERE inst = 2 AND obsID = 1342188189
@@ -70,3 +136,14 @@ WHERE inst = 2 AND obsType = 2 AND pointingMode = 2
 SELECT *
 FROM load.rawPointing 
 WHERE inst = 2 AND obsID = 1342252289
+
+
+SELECT *
+FROM load.RAwObservation
+WHERE inst = 2 AND obsType = 2 AND pointingMode = 2 AND obsID = 1342227519
+
+
+
+SELECT *
+FROM load.RAwObservation
+WHERE inst = 2 AND obsType = 2 AND pointingMode = 2 AND obsID = 1342227454
