135cdfd955838deeeec6c860d0d90471b4ec7d9b
Laszlo Dobos <dobos@complex.elte.hu>
2015-10-15 18:01:17 +0200

    Merged flag load scripts.

--- load/load_quality.sh
+++ load/load_quality.sh
@@ -5,3 +5,4 @@ DB="Herschel_3"
 DIR="../quality"
 
 bcp load.ObsQuality in "$DIR/quality.dat" -S$SERVER -d$DB -T -c -t" " -r"0x0A"
+bcp load.ObsSso in "$DIR/sso.dat" -S$SERVER -d$DB -T -c -t" " -r"0x0A"
