6764148ffd2e50083d09e930eb2a26757bef0a0e
Laszlo Dobos <dobos@complex.elte.hu>
2015-10-19 15:36:26 +0200

    Updated failed flags to work with PACS.

--- load/load_flags.sh
+++ load/load_flags.sh
@@ -2,7 +2,7 @@
 
 SERVER="FUTURE1"
 DB="Herschel_3"
-DIR="../quality"
+DIR="../flags"
 
 bcp load.ObsQuality in "$DIR/quality.dat" -S$SERVER -d$DB -T -c -t" " -r"0x0A"
 bcp load.ObsSso in "$DIR/sso.dat" -S$SERVER -d$DB -T -c -t" " -r"0x0A"
--- load/prepare_flags.sh
+++ load/prepare_flags.sh
@@ -6,7 +6,7 @@ OUTDIR="../flags"
 mkdir -p "$OUTDIR"
 rm "$OUTDIR/quality.dat"
 
-#tail "$INDIR/quality/quality_pacs.txt"          -n+5 | awk '{print "1", $1, $2=="FAILED"?1:0}' >> "$OUTDIR/quality.dat"
+tail "$INDIR/quality/quality_pacs.txt"           -n+5 | awk '{print "1", $1, $2=="FAILED"?1:0}' >> "$OUTDIR/quality.dat"
 tail "$INDIR/quality/quality_parallel_pacs.txt"  -n+5 | awk '{print "1", $1, $2=="FAILED"?1:0}' >> "$OUTDIR/quality.dat"
 tail "$INDIR/quality/quality_spire.txt"          -n+5 | awk '{print "2", $1, $2=="FAILED"?1:0}' >> "$OUTDIR/quality.dat"
 tail "$INDIR/quality/quality_parallel_spire.txt" -n+5 | awk '{print "2", $1, $2=="FAILED"?1:0}' >> "$OUTDIR/quality.dat"
