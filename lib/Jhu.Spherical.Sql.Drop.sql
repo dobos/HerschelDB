01d4673d24528f083feeb07b347d365fe0c88e85
Laszlo Dobos <dobos@complex.elte.hu>
2016-03-09 17:20:39 +0100

    Updated spherical lib.

--- lib/Jhu.Spherical.Sql.Drop.sql
+++ lib/Jhu.Spherical.Sql.Drop.sql
@@ -1,103 +1,135 @@
 
-IF (OBJECT_ID('htm.FromXyz') IS NOT NULL)
+IF (OBJECT_ID('region.ConvexHullXyz') IS NOT NULL)
 BEGIN
-    DROP FUNCTION [htm].[FromXyz]
+    DROP AGGREGATE [region].[ConvexHullXyz]
 END
 
 GO
 
 
-IF (OBJECT_ID('htm.FromEq') IS NOT NULL)
+IF (OBJECT_ID('region.ConvexHullEq') IS NOT NULL)
 BEGIN
-    DROP FUNCTION [htm].[FromEq]
+    DROP AGGREGATE [region].[ConvexHullEq]
 END
 
 GO
 
 
-IF (OBJECT_ID('htm.GetCenter') IS NOT NULL)
+IF (OBJECT_ID('region.IntersectEvery') IS NOT NULL)
 BEGIN
-    DROP FUNCTION [htm].[GetCenter]
+    DROP AGGREGATE [region].[IntersectEvery]
 END
 
 GO
 
 
-IF (OBJECT_ID('htm.GetCorners') IS NOT NULL)
+IF (OBJECT_ID('region.UnionEvery') IS NOT NULL)
 BEGIN
-    DROP FUNCTION [htm].[GetCorners]
+    DROP AGGREGATE [region].[UnionEvery]
 END
 
 GO
 
 
-IF (OBJECT_ID('htm.Parse') IS NOT NULL)
+IF (OBJECT_ID('point.AvgXyz') IS NOT NULL)
 BEGIN
-    DROP FUNCTION [htm].[Parse]
+    DROP AGGREGATE [point].[AvgXyz]
 END
 
 GO
 
 
-IF (OBJECT_ID('htm.ToString') IS NOT NULL)
+IF (OBJECT_ID('point.AvgEq') IS NOT NULL)
 BEGIN
-    DROP FUNCTION [htm].[ToString]
+    DROP AGGREGATE [point].[AvgEq]
 END
 
 GO
 
 
-IF (OBJECT_ID('htm.Cover') IS NOT NULL)
+IF (OBJECT_ID('htmid.Parse') IS NOT NULL)
 BEGIN
-    DROP FUNCTION [htm].[Cover]
+    DROP FUNCTION [htmid].[Parse]
 END
 
 GO
 
 
-IF (OBJECT_ID('htm.CoverAdvanced') IS NOT NULL)
+IF (OBJECT_ID('htmid.ToString') IS NOT NULL)
 BEGIN
-    DROP FUNCTION [htm].[CoverAdvanced]
+    DROP FUNCTION [htmid].[ToString]
 END
 
 GO
 
 
-IF (OBJECT_ID('htm.CoverRegion') IS NOT NULL)
+IF (OBJECT_ID('htmid.FromXyz') IS NOT NULL)
 BEGIN
-    DROP FUNCTION [htm].[CoverRegion]
+    DROP FUNCTION [htmid].[FromXyz]
 END
 
 GO
 
 
-IF (OBJECT_ID('htm.CoverCircleXyz') IS NOT NULL)
+IF (OBJECT_ID('htmid.FromEq') IS NOT NULL)
 BEGIN
-    DROP FUNCTION [htm].[CoverCircleXyz]
+    DROP FUNCTION [htmid].[FromEq]
 END
 
 GO
 
 
-IF (OBJECT_ID('htm.CoverCircleEq') IS NOT NULL)
+IF (OBJECT_ID('htmid.FromRange') IS NOT NULL)
 BEGIN
-    DROP FUNCTION [htm].[CoverCircleEq]
+    DROP FUNCTION [htmid].[FromRange]
 END
 
 GO
 
 
-IF (OBJECT_ID('point.ConvertEqToXyz') IS NOT NULL)
+IF (OBJECT_ID('htmid.GetLevel') IS NOT NULL)
 BEGIN
-    DROP FUNCTION [point].[ConvertEqToXyz]
+    DROP FUNCTION [htmid].[GetLevel]
 END
 
 GO
 
 
-IF (OBJECT_ID('point.ConvertXyzToEq') IS NOT NULL)
+IF (OBJECT_ID('htmid.GetCenter') IS NOT NULL)
 BEGIN
-    DROP FUNCTION [point].[ConvertXyzToEq]
+    DROP FUNCTION [htmid].[GetCenter]
+END
+
+GO
+
+
+IF (OBJECT_ID('htmid.GetCorners') IS NOT NULL)
+BEGIN
+    DROP FUNCTION [htmid].[GetCorners]
+END
+
+GO
+
+
+IF (OBJECT_ID('htmid.GetTriangle') IS NOT NULL)
+BEGIN
+    DROP FUNCTION [htmid].[GetTriangle]
+END
+
+GO
+
+
+IF (OBJECT_ID('point.EqToXyz') IS NOT NULL)
+BEGIN
+    DROP FUNCTION [point].[EqToXyz]
+END
+
+GO
+
+
+IF (OBJECT_ID('point.XyzToEq') IS NOT NULL)
+BEGIN
+    DROP FUNCTION [point].[XyzToEq]
 END
 
 GO
@@ -119,22 +151,6 @@ END
 GO
 
 
-IF (OBJECT_ID('region.GetHalfspaces') IS NOT NULL)
-BEGIN
-    DROP FUNCTION [region].[GetHalfspaces]
-END
-
-GO
-
-
-IF (OBJECT_ID('region.ToString') IS NOT NULL)
-BEGIN
-    DROP FUNCTION [region].[ToString]
-END
-
-GO
-
-
 IF (OBJECT_ID('region.Parse') IS NOT NULL)
 BEGIN
     DROP FUNCTION [region].[Parse]
@@ -151,81 +167,41 @@ END
 GO
 
 
-IF (OBJECT_ID('region.GetArea') IS NOT NULL)
+IF (OBJECT_ID('region.ToString') IS NOT NULL)
 BEGIN
-    DROP FUNCTION [region].[GetArea]
+    DROP FUNCTION [region].[ToString]
 END
 
 GO
 
 
-IF (OBJECT_ID('region.GetPatches') IS NOT NULL)
+IF (OBJECT_ID('region.ToUdt') IS NOT NULL)
 BEGIN
-    DROP FUNCTION [region].[GetPatches]
+    DROP FUNCTION [region].[ToUdt]
 END
 
 GO
 
 
-IF (OBJECT_ID('region.Simplify') IS NOT NULL)
+IF (OBJECT_ID('region.HasError') IS NOT NULL)
 BEGIN
-    DROP FUNCTION [region].[Simplify]
+    DROP FUNCTION [region].[HasError]
 END
 
 GO
 
 
-IF (OBJECT_ID('region.SimplifyAdvanced') IS NOT NULL)
+IF (OBJECT_ID('region.Error') IS NOT NULL)
 BEGIN
-    DROP FUNCTION [region].[SimplifyAdvanced]
+    DROP FUNCTION [region].[Error]
 END
 
 GO
 
 
-IF (OBJECT_ID('region.GetArcs') IS NOT NULL)
+IF (OBJECT_ID('region.Area') IS NOT NULL)
 BEGIN
-    DROP FUNCTION [region].[GetArcs]
-END
-
-GO
-
-
-IF (OBJECT_ID('region.GetOutlineArcs') IS NOT NULL)
-BEGIN
-    DROP FUNCTION [region].[GetOutlineArcs]
-END
-
-GO
-
-
-IF (OBJECT_ID('region.GetConvexes') IS NOT NULL)
-BEGIN
-    DROP FUNCTION [region].[GetConvexes]
-END
-
-GO
-
-
-IF (OBJECT_ID('region.Grow') IS NOT NULL)
-BEGIN
-    DROP FUNCTION [region].[Grow]
-END
-
-GO
-
-
-IF (OBJECT_ID('region.GrowAdvanced') IS NOT NULL)
-BEGIN
-    DROP FUNCTION [region].[GrowAdvanced]
-END
-
-GO
-
-
-IF (OBJECT_ID('region.AddHalfspace') IS NOT NULL)
-BEGIN
-    DROP FUNCTION [region].[AddHalfspace]
+    DROP FUNCTION [region].[Area]
 END
 
 GO
@@ -263,22 +239,6 @@ END
 GO
 
 
-IF (OBJECT_ID('region.Intersect') IS NOT NULL)
-BEGIN
-    DROP FUNCTION [region].[Intersect]
-END
-
-GO
-
-
-IF (OBJECT_ID('region.IntersectAdvanced') IS NOT NULL)
-BEGIN
-    DROP FUNCTION [region].[IntersectAdvanced]
-END
-
-GO
-
-
 IF (OBJECT_ID('region.Union') IS NOT NULL)
 BEGIN
     DROP FUNCTION [region].[Union]
@@ -295,65 +255,169 @@ END
 GO
 
 
-IF (OBJECT_ID('region.GetErrorMessage') IS NOT NULL)
+IF (OBJECT_ID('region.Intersect') IS NOT NULL)
 BEGIN
-    DROP FUNCTION [region].[GetErrorMessage]
+    DROP FUNCTION [region].[Intersect]
 END
 
 GO
 
 
-IF (OBJECT_ID('region.HasError') IS NOT NULL)
+IF (OBJECT_ID('region.IntersectAdvanced') IS NOT NULL)
 BEGIN
-    DROP FUNCTION [region].[HasError]
+    DROP FUNCTION [region].[IntersectAdvanced]
 END
 
 GO
 
 
-IF (OBJECT_ID('region.GetConvexHull') IS NOT NULL)
+IF (OBJECT_ID('region.Simplify') IS NOT NULL)
 BEGIN
-    DROP FUNCTION [region].[GetConvexHull]
+    DROP FUNCTION [region].[Simplify]
 END
 
 GO
 
 
-IF (OBJECT_ID('region.Reduce') IS NOT NULL)
+IF (OBJECT_ID('region.SimplifyAdvanced') IS NOT NULL)
 BEGIN
-    DROP FUNCTION [region].[Reduce]
+    DROP FUNCTION [region].[SimplifyAdvanced]
 END
 
 GO
 
 
-IF (OBJECT_ID('region.UnionEvery') IS NOT NULL)
+IF (OBJECT_ID('region.Grow') IS NOT NULL)
 BEGIN
-    DROP AGGREGATE [region].[UnionEvery]
+    DROP FUNCTION [region].[Grow]
 END
 
 GO
 
 
-IF (OBJECT_ID('region.IntersectEvery') IS NOT NULL)
+IF (OBJECT_ID('region.ConvexHull') IS NOT NULL)
 BEGIN
-    DROP AGGREGATE [region].[IntersectEvery]
+    DROP FUNCTION [region].[ConvexHull]
 END
 
 GO
 
 
-IF (OBJECT_ID('region.ConvexHullXyz') IS NOT NULL)
+IF (OBJECT_ID('region.AddHalfspace') IS NOT NULL)
 BEGIN
-    DROP AGGREGATE [region].[ConvexHullXyz]
+    DROP FUNCTION [region].[AddHalfspace]
 END
 
 GO
 
 
-IF (OBJECT_ID('region.ConvexHullEq') IS NOT NULL)
+IF (OBJECT_ID('region.GetArcs') IS NOT NULL)
 BEGIN
-    DROP AGGREGATE [region].[ConvexHullEq]
+    DROP FUNCTION [region].[GetArcs]
+END
+
+GO
+
+
+IF (OBJECT_ID('region.GetOutlineArcs') IS NOT NULL)
+BEGIN
+    DROP FUNCTION [region].[GetOutlineArcs]
+END
+
+GO
+
+
+IF (OBJECT_ID('region.GetConvexes') IS NOT NULL)
+BEGIN
+    DROP FUNCTION [region].[GetConvexes]
+END
+
+GO
+
+
+IF (OBJECT_ID('region.GetHalfspaces') IS NOT NULL)
+BEGIN
+    DROP FUNCTION [region].[GetHalfspaces]
+END
+
+GO
+
+
+IF (OBJECT_ID('region.GetPatches') IS NOT NULL)
+BEGIN
+    DROP FUNCTION [region].[GetPatches]
+END
+
+GO
+
+
+IF (OBJECT_ID('htm.Cover') IS NOT NULL)
+BEGIN
+    DROP FUNCTION [htm].[Cover]
+END
+
+GO
+
+
+IF (OBJECT_ID('htm.CoverAdvanced') IS NOT NULL)
+BEGIN
+    DROP FUNCTION [htm].[CoverAdvanced]
+END
+
+GO
+
+
+IF (OBJECT_ID('htm.CoverString') IS NOT NULL)
+BEGIN
+    DROP FUNCTION [htm].[CoverString]
+END
+
+GO
+
+
+IF (OBJECT_ID('htm.CoverCircleXyz') IS NOT NULL)
+BEGIN
+    DROP FUNCTION [htm].[CoverCircleXyz]
+END
+
+GO
+
+
+IF (OBJECT_ID('htm.CoverCircleEq') IS NOT NULL)
+BEGIN
+    DROP FUNCTION [htm].[CoverCircleEq]
+END
+
+GO
+
+
+IF (OBJECT_ID('arc.GreatCircleEq') IS NOT NULL)
+BEGIN
+    DROP FUNCTION [arc].[GreatCircleEq]
+END
+
+GO
+
+
+IF (TYPE_ID('dbo.Region') IS NOT NULL)
+BEGIN
+    DROP TYPE [dbo].[Region]
+END
+
+GO
+
+
+IF (TYPE_ID('dbo.Arc') IS NOT NULL)
+BEGIN
+    DROP TYPE [dbo].[Arc]
+END
+
+GO
+
+
+IF (TYPE_ID('dbo.Point') IS NOT NULL)
+BEGIN
+    DROP TYPE [dbo].[Point]
 END
 
 GO
@@ -374,7 +438,7 @@ DROP ASSEMBLY [Jhu.Spherical]
 GO
 
 
-DROP SCHEMA [htm]
+DROP SCHEMA [htmid]
 
 GO
 
@@ -385,6 +449,16 @@ GO
 
 
 DROP SCHEMA [region]
+
+GO
+
+
+DROP SCHEMA [htm]
+
+GO
+
+
+DROP SCHEMA [arc]
 
 GO
 
